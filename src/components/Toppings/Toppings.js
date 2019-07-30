ed75c3df8fd69c28916e08ec9398265be68508e3
Thiago Miranda <thiagomir@gmail.com>
2019-07-29 23:43:54 -0300

    fix default selected

--- src/components/Toppings/Toppings.js
+++ src/components/Toppings/Toppings.js
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Form, Checkbox, Loader, Segment } from 'semantic-ui-react'
 import { useQuery } from 'graphql-hooks'
  
@@ -22,10 +22,14 @@ export default function Toppings({ selectTopping, selectedToppings, size }) {
     }
   })
   let element
+
+  useEffect(() => {
+    if(data) selectTopping(false, data.pizzaSizeByName.toppings.find(({ defaultSelected }) => defaultSelected).topping)
+  }, [data])
   
   if (loading) element = <Segment><Loader active inline='centered' /></Segment>
   if (data) element = data.pizzaSizeByName.toppings.map(({ topping, defaultSelected }) => {
-    const checked = defaultSelected || selectedToppings.join().indexOf(topping.name) > -1
+    const checked = selectedToppings.join().indexOf(topping.name) > -1
     
     return (
       <Form.Field key={topping.name} >
